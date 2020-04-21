107e236efb624133ecad9572cf02ec31ee6aaea2
regfish7 <60451516+regfish7@users.noreply.github.com>
2020-04-21 19:14:59 -0400

    This commit finishes the steps in run_analysis, includes the Code Book, a finished README file and the completed tidy dataset (tidydata.csv).

--- README.md
+++ README.md
@@ -1,19 +1,35 @@
 # get_clean_data
 This repository saves the final project for the "Getting and Cleaning Data" Course in the JHU Data Science Specialization through Coursera.
 
-=================================================================================
-DATA
+## README ##
 
-The data for this project comes from the "Human Activity Recognition Using Smartphones Dataset - Version 1.0", available at https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip . Information about how this data was collected can be found at http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones . In summary, the data comes from an experiment of 30 volunteers between the ages of 19-48. The participants performed 6 different activities (WALKING, WALKING_UPSTAIRS, WALKING_DOWNSTAIRS, SITTING, STANDING, LAYING) while wearing a Samsun Galaxy S II and then accelerometer and gyroscope data was collected. 
+**DATA**
 
-=================================================================================
-CONTENTS
+The data for this project comes from the "Human Activity Recognition Using Smartphones Dataset - Version 1.0", available through the linked [UCI HAR Dataset](https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip). Information about how this data was collected can be found at the [Human Activity Recognition Using Smartphones](http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones) website. In summary, the data comes from an experiment of 30 volunteers between the ages of 19-48. The participants performed 6 different activities (WALKING, WALKING_UPSTAIRS, WALKING_DOWNSTAIRS, SITTING, STANDING, LAYING) while wearing a Samsun Galaxy S II and then accelerometer and gyroscope data was collected. 
+
+The citation for this dataset is: 
+
+[1] Davide Anguita, Alessandro Ghio, Luca Oneto, Xavier Parra and Jorge L. Reyes-Ortiz. Human Activity Recognition on Smartphones using a Multiclass Hardware-Friendly Support Vector Machine. International Workshop of Ambient Assisted Living (IWAAL 2012). Vitoria-Gasteiz, Spain. Dec 2012
+
+**CONTENTS**
 
 This repository contains the following files:
- - 'README.txt'
- - 
+
+* README.md
+* run_analysis.R
+* CodeBook.md
+* tidydata.csv
  
  The descriptions of the files are as follows:
  
-=================================================================================
-NOTES
+* *README.md*: Gives an overview of the files in the project and the dataset itself
+* *run_analysis.R*: This script creates a tidy dataset from the UCI HAR Dataset zip file listed above
+ (under "DATA") by:
+ 
+    * Merging the training and tests sets to create one data set
+    * Extracting only the data points that give the mean and standard deviation of each measurement
+    * Renaming the activities and variables to be more descriptive
+    * Creating a second tidy data set with the average of each variable for each activity and each subject. 
+ * *CodeBook.md*: This code book describes the variables, the data, and the transformations used to produced tidydata.csv, the cleaned data. 
+ * *tidydata.csv*: The tidy dataset produced in run_analysis.R. Each row represents the average readings for each measurement for a given subject and activity. 
+ 
--- run_analysis.R
+++ run_analysis.R
@@ -4,12 +4,20 @@
 train <- read.table('../UCI HAR Dataset/train/X_train.txt',header = FALSE,sep = "", dec = ".")
 #train is a 7352 x 561 set -> the columns correspond to the 561 variables in features.txt
 #The rows correspond to which subject the measurement was taken from (see subject_train.txt)
+#The dataset y_train (7352 x 1) gives the activity label for each row
+
 #Create new column called "Subject" to indicate which subject the measurement was taken from
 subject <- read.table('../UCI HAR Dataset/train/subject_train.txt',header = FALSE, sep ="")
-train$subject <- subject #for some reason called "subject.V1"
+train$subject <- subject 
 train$subject <-as.numeric(unlist(train$subject)) #need to turn column type from "dataframe" to "numeric"
 #put the subject column first:
 train <- train[,c(ncol(train),1:(ncol(train)-1))]
+
+#Create a new column called "Activity" that gives the activity label for each row
+train_labels <- read.table('../UCI HAR Dataset/train/y_train.txt')
+train$activity <- train_labels
+train$activity <- as.numeric(unlist(train$activity))
+train <- train[,c(1, ncol(train), 2:(ncol(train)-1))] #put activity column second
 
 # Repeat process for the test set
 test <- read.table('../UCI HAR Dataset/test/X_test.txt',header = FALSE,sep = "", dec = ".")
@@ -17,6 +25,10 @@ subject_test <- read.table('../UCI HAR Dataset/test/subject_test.txt',header = F
 test$subject <- subject_test 
 test$subject <-as.numeric(unlist(test$subject))
 test <- test[,c(ncol(test),1:(ncol(test)-1))]
+test_labels <- read.table('../UCI HAR Dataset/test/y_test.txt')
+test$activity <- test_labels
+test$activity <- as.numeric(unlist(test$activity))
+test <- test[,c(1, ncol(test), 2:(ncol(test)-1))] #put activity column second
 
 # Merge the two datasets
 merged_data <-rbind(train,test)
@@ -27,12 +39,43 @@ features <- read.table('../UCI HAR Dataset/features.txt',header = FALSE,sep = ""
 features$V2 <- as.character(features$V2) #convert factor data type to string
 colInds <- grep("mean|std",features$V2, ignore.case = TRUE)
 
-subset_data <- merged_data[,c(1,colInds+1)] #take only columns with mean or std (add 1 b/c subject is now col 1)
+subset_data <- merged_data[,c(1:2,colInds+2)] #take only columns with mean or std (add 2 b/c subject and activity cols added)
 
 ########################### Rename the activies in the data set #####################################
-
+# These activity names are based on the file activity_labels.txt
+activity_labels <- read.table('../UCI HAR Dataset/activity_labels.txt')
+subset_data$activity <- activity_labels[match(subset_data$activity, activity_labels$V1),]$V2
 
 ###################### Label the data set with descriptive variable names ###########################
+#First, import variable names given in features.txt
+feature_names <- read.table('../UCI HAR Dataset/features.txt')
+
+#Rename these variables so that they are more descriptive
+feature_names$V2 <- gsub('^t','Time',feature_names$V2)
+feature_names$V2 <- gsub('^f','Freq',feature_names$V2)
+feature_names$V2 <- gsub('[[:punct:]]+','',feature_names$V2)
+feature_names$V2 <- gsub('mean','Mean', feature_names$V2)
+feature_names$V2 <- gsub('std','Std', feature_names$V2)
+
+#Rename columns with the updated feature names
+#first, change column names to just be numeric to match feature_names codes
+colnames(subset_data)<- gsub('V','',colnames(subset_data))
+
+#rename
+for(name in colnames(subset_data[3:length(subset_data)])){
+    names(subset_data)[names(subset_data)==name]<-feature_names[feature_names$V1==name,'V2']
+}
 
 
+    
 ## Create a second tidy data set with the average of each variable for each activity and each subject
+tidy_data = aggregate(subset_data, by= list(subset_data$subject,subset_data$activity),FUN = mean)
+tidy_data <-rename(tidy_data,replace = c('Group.1'='Subject', 'Group.2'='Activity' ))
+tidy_data <- subset(tidy_data, select = -c(subject,activity))
+tidy_data <- tidy_data[order(tidy_data$Subject),]
+
+#Re-index row
+row.names(tidy_data) <- 1:nrow(tidy_data)
+
+## Export the dataset
+write.csv(tidy_data, file = 'tidydata.csv',row.names=FALSE)
